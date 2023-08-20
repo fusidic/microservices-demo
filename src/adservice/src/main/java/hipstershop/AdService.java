10fdaee2491a88f0e8171764e926da68cdadbcd1
fusidic <arithbar@gmail.com>
2023-08-20 12:36:00 +0800

    add special text in advertise

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -150,37 +150,37 @@ public final class AdService {
     Ad hairdryer =
         Ad.newBuilder()
             .setRedirectUrl("/product/2ZYFJ3GM2N")
-            .setText("Hairdryer for sale. 50% off.")
+            .setText("Discount in GuangZhou: Hairdryer for sale. 50% off.")
             .build();
     Ad tankTop =
         Ad.newBuilder()
             .setRedirectUrl("/product/66VCHSJNUP")
-            .setText("Tank top for sale. 20% off.")
+            .setText("Discount in GuangZhou: Tank top for sale. 20% off.")
             .build();
     Ad candleHolder =
         Ad.newBuilder()
             .setRedirectUrl("/product/0PUK6V6EV0")
-            .setText("Candle holder for sale. 30% off.")
+            .setText("Discount in GuangZhou: Candle holder for sale. 30% off.")
             .build();
     Ad bambooGlassJar =
         Ad.newBuilder()
             .setRedirectUrl("/product/9SIQT8TOJO")
-            .setText("Bamboo glass jar for sale. 10% off.")
+            .setText("Discount in GuangZhou: Bamboo glass jar for sale. 10% off.")
             .build();
     Ad watch =
         Ad.newBuilder()
             .setRedirectUrl("/product/1YMWWN1N4O")
-            .setText("Watch for sale. Buy one, get second kit for free")
+            .setText("Discount in GuangZhou: Watch for sale. Buy one, get second kit for free")
             .build();
     Ad mug =
         Ad.newBuilder()
             .setRedirectUrl("/product/6E92ZMYYFZ")
-            .setText("Mug for sale. Buy two, get third one for free")
+            .setText("Discount in GuangZhou: Mug for sale. Buy two, get third one for free")
             .build();
     Ad loafers =
         Ad.newBuilder()
             .setRedirectUrl("/product/L9ECAV7KIM")
-            .setText("Loafers for sale. Buy one, get second one for free")
+            .setText("Discount in GuangZhou: Loafers for sale. Buy one, get second one for free")
             .build();
     return ImmutableListMultimap.<String, Ad>builder()
         .putAll("clothing", tankTop)
